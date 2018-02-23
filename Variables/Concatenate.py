1ccf9a5608d006c0e064bbdceb82d83356a85c01
DgrinderHZ <32304244+DgrinderHZ@users.noreply.github.com>
2018-02-23 04:26:30 +0000

    Create Concatenate.py
    Add String Concatination

---
+++ Variables/Concatenate.py
@@ -0,0 +1,16 @@
+# Lesson 2.2: Strings
+
+# Strings are sequences of characters that are enclosed in quotes.
+# We can enclose them with single or double quotes and assign them
+# to variables. We can even combine strings by adding them (we call
+# this concatenation).
+
+print 'Hello'
+print "Hello"
+
+hello = "Howdy"
+print hello
+
+# Add your own code and notes here
+answer = "Hello " + hello 
+print answer
