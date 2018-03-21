4ed60d8c14512022f3e6195494065b4c6deb0c5a
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-21 20:14:28 +0000

    Add spirit_age

---
+++ variables/spirit_age.py
@@ -0,0 +1,11 @@
+"""
+Write a program thet defines the variable - age -
+and prints out the number of days lived!
+"""
+
+def spirit_age(age):
+	days_in_year = 365.25
+	days_alive = age *  days_in_year
+	return days_alive
+
+print spirit_age(7)
